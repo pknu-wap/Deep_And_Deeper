2cb8dfba75dcd64f00bf7dc8f6a4fb8edfe28466
alsdddk <alsdk__90@naver.com>
2023-10-03 19:52:05 +0900

    점프/구르기 최종

--- Unity/Assets/BT/Roll.cs
+++ Unity/Assets/BT/Roll.cs
@@ -8,7 +8,7 @@ namespace BT
     [MBTNode(name = "Hero/Roll")]
     public class Roll : Leaf
     {
-        private float moveSpeed = 7f;
+        [SerializeField] private float moveSpeed = 7f;
         private Animator _animator;
         private SpriteRenderer _spriteRenderer;
         private Rigidbody2D _rigidbody2D;
@@ -23,9 +23,8 @@ namespace BT
         public override NodeResult Execute()
         {
             var inputX = Input.GetAxis("Horizontal");
-            _rigidbody2D.velocity = new Vector2( inputX * moveSpeed, _rigidbody2D.velocity.y);
             
-            if(inputX == 0)
+            
                 switch (_spriteRenderer.flipX)
                 {
                     case true:
@@ -38,6 +37,7 @@ namespace BT
                 //_rigidbody2D.velocity = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
             
             _animator.Play("Roll");
+            Debug.Log("Roll!");
             return NodeResult.success;
         }
     }
