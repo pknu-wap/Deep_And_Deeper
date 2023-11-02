38d1bc687ff18eb442b0a7ea41c9863aba0c73f9
이문빈 <3412mb@gmail.com>
2023-11-02 19:19:51 +0900

    Roll에서 Collider 참조 제거

--- Unity/Assets/BT/Roll.cs
+++ Unity/Assets/BT/Roll.cs
@@ -1,4 +1,3 @@
-using System;
 using MBT;
 using UnityEngine;
 
@@ -12,8 +11,7 @@ namespace BT
         private Animator _animator;
         private SpriteRenderer _spriteRenderer;
         private Rigidbody2D _rigidbody2D;
-        private BoxCollider2D _boxCollider2D;
-        
+
         public Vector2 originalSize;
         public Vector2 originalOffset;
 
@@ -22,30 +20,23 @@ namespace BT
             _animator = GetComponent<Animator>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _boxCollider2D = GetComponent<BoxCollider2D>();
-            
-            originalSize = _boxCollider2D.size;
-            originalOffset = _boxCollider2D.offset;
-            
         }
 
         public override NodeResult Execute()
         {
             var inputX = Input.GetAxis("Horizontal");
-                switch (_spriteRenderer.flipX)
-                {
-                    case true:
-                        _rigidbody2D.velocity = new Vector2(-moveSpeed, _rigidbody2D.velocity.y);
-                        break;
-                    case false:
-                        _rigidbody2D.velocity = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
-                        break;
-                }
-            
+            switch (_spriteRenderer.flipX)
+            {
+                case true:
+                    _rigidbody2D.velocity = new Vector2(-moveSpeed, _rigidbody2D.velocity.y);
+                    break;
+                case false:
+                    _rigidbody2D.velocity = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
+                    break;
+            }
+
             _animator.Play("Roll");
-            _boxCollider2D.size = new Vector2(_boxCollider2D.size.x, 0.6f);
-            _boxCollider2D.offset = new Vector2(_boxCollider2D.offset.x, 0.4f);
-            
+
             return NodeResult.success;
         }
     }
