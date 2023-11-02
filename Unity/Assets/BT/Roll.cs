49a3c14d5e2c1ffd37709319dbab493b234208c3
이문빈 <3412mb@gmail.com>
2023-11-02 19:19:51 +0900

    Roll에서 BoxCollider2D 참조 제거

--- Unity/Assets/BT/Roll.cs
+++ Unity/Assets/BT/Roll.cs
@@ -1,4 +1,3 @@
-using System;
 using MBT;
 using UnityEngine;
 
@@ -12,7 +11,6 @@ namespace BT
         private Animator _animator;
         private SpriteRenderer _spriteRenderer;
         private Rigidbody2D _rigidbody2D;
-        private BoxCollider2D _boxCollider2D;
         
         public Vector2 originalSize;
         public Vector2 originalOffset;
@@ -22,10 +20,6 @@ namespace BT
             _animator = GetComponent<Animator>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _boxCollider2D = GetComponent<BoxCollider2D>();
-            
-            originalSize = _boxCollider2D.size;
-            originalOffset = _boxCollider2D.offset;
             
         }
 
@@ -43,9 +37,6 @@ namespace BT
                 }
             
             _animator.Play("Roll");
-            _boxCollider2D.size = new Vector2(_boxCollider2D.size.x, 0.6f);
-            _boxCollider2D.offset = new Vector2(_boxCollider2D.offset.x, 0.4f);
-            
             return NodeResult.success;
         }
     }
