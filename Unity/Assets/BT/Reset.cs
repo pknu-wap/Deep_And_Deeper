01e2304ebe567eb240820a94c2f85e767621dc86
이문빈 <3412mb@gmail.com>
2023-11-03 19:53:13 +0900

    Reset 노드 기능 모두 제거

--- Unity/Assets/BT/Reset.cs
+++ Unity/Assets/BT/Reset.cs
@@ -1,4 +1,3 @@
-using System;
 using MBT;
 using UnityEngine;
 
@@ -6,20 +5,10 @@ namespace BT
 {
     [AddComponentMenu("")]
     [MBTNode(name = "Hero/Reset")]
-    public class Reset: Leaf
+    public class Reset : Leaf
     {
-        private BoxCollider2D _boxCollider2D;
-
-        private void Start()
-        {
-            _boxCollider2D = GetComponent<BoxCollider2D>();
-        }
-
         public override NodeResult Execute()
         {
-            _boxCollider2D.size = new Vector2(_boxCollider2D.size.x, 1.3f);
-            _boxCollider2D.offset = new Vector2(_boxCollider2D.offset.x, 0.7f);
-            
             return NodeResult.success;
         }
     }
