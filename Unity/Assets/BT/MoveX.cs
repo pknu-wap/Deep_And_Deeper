0f5817ef2ea433bf1e14c00f6fcb45b0e06af5f3
이문빈 <3412mb@gmail.com>
2023-11-07 11:24:45 +0900

    MoveX 노드의 기능을 Run 노드로 이전하여 통합

--- Unity/Assets/BT/MoveX.cs
+++ Unity/Assets/BT/MoveX.cs
@@ -7,25 +7,8 @@ namespace BT
     [MBTNode(name = "Hero/MoveX")]
     public class MoveX : Leaf
     {
-        [SerializeField] private float moveSpeed;
-        private Rigidbody2D _rigidbody2D;
-        private SpriteRenderer _spriteRenderer;
-
-        private void Start()
-        {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            _rigidbody2D = GetComponent<Rigidbody2D>();
-        }
-
         public override NodeResult Execute()
         {
-            var inputX = Input.GetAxis("Horizontal");
-
-            
-            _rigidbody2D.velocity = new Vector2(inputX * moveSpeed, _rigidbody2D.velocity.y);
-            
-
-            if (inputX == 0) return NodeResult.failure;
             return NodeResult.success;
         }
     }
--- Unity/Assets/BT/Run.cs
+++ Unity/Assets/BT/Run.cs
@@ -7,23 +7,24 @@ namespace BT
     [MBTNode(name = "Hero/Run")]
     public class Run : Leaf
     {
-        private Animator _animator;
+        [SerializeField] private float runSpeed = 5f;
+        private Rigidbody2D _rigidbody2D;
         private SpriteRenderer _spriteRenderer;
 
         private void Start()
         {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
-            _animator = GetComponent<Animator>();
         }
 
         public override NodeResult Execute()
         {
             var inputX = Input.GetAxis("Horizontal");
-
             if (inputX == 0) return NodeResult.failure;
-
-            _animator.Play("Run");
+            
             _spriteRenderer.flipX = inputX < 0;
+            _rigidbody2D.velocity = new Vector2(inputX * runSpeed, _rigidbody2D.velocity.y);
+            
             return NodeResult.success;
         }
     }
