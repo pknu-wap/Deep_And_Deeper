dbff3ecf684959d7fdaf2ccfcda687f5367716bf
이문빈 <3412mb@gmail.com>
2023-11-07 20:24:49 +0900

    MoveX 노드 기능 삭제 후 Run 노드에 통합

--- Unity/Assets/Hero/Nodes/MoveX.cs
+++ Unity/Assets/Hero/Nodes/MoveX.cs
@@ -7,14 +7,8 @@ namespace Hero.Nodes
     [MBTNode(name = "Hero/MoveX")]
     public class MoveX : Leaf
     {
-        [SerializeField] private float moveSpeed = 5;
-
         public override NodeResult Execute()
         {
-            var inputX = Input.GetAxis("Horizontal");
-            if (inputX == 0) return NodeResult.failure;
-
-            HeroManager.Instance.SetVelocity(inputX * moveSpeed, null);
             return NodeResult.success;
         }
     }
