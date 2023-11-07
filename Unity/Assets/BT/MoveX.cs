8704ed6b36804cea8775e76bb8f9174c1a843d24
이문빈 <3412mb@gmail.com>
2023-11-07 16:44:23 +0900

    MoveX 노드가 HeroManager를 참조하도록 변경

--- Unity/Assets/BT/MoveX.cs
+++ Unity/Assets/BT/MoveX.cs
@@ -1,3 +1,4 @@
+using Hero;
 using MBT;
 using UnityEngine;
 
@@ -8,24 +9,13 @@ namespace BT
     public class MoveX : Leaf
     {
         [SerializeField] private float moveSpeed;
-        private Rigidbody2D _rigidbody2D;
-        private SpriteRenderer _spriteRenderer;
-
-        private void Start()
-        {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            _rigidbody2D = GetComponent<Rigidbody2D>();
-        }
 
         public override NodeResult Execute()
         {
             var inputX = Input.GetAxis("Horizontal");
-
-            
-            _rigidbody2D.velocity = new Vector2(inputX * moveSpeed, _rigidbody2D.velocity.y);
-            
-
             if (inputX == 0) return NodeResult.failure;
+            
+            HeroManager.Instance.SetVelocity(inputX * moveSpeed, null);
             return NodeResult.success;
         }
     }
