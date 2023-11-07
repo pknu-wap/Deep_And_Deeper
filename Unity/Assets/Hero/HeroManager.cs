ac161f714a6e12437c13d15579abc524f398bffd
이문빈 <3412mb@gmail.com>
2023-11-07 14:18:02 +0900

    HeroManager 싱글턴 구현

---
+++ Unity/Assets/Hero/HeroManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class HeroManager
+{
+    private static HeroManager _instance;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    public static HeroManager Instance => _instance ??= new HeroManager();
+
+    private HeroManager()
+    {
+        var gameObject = GameObject.FindWithTag("Player");
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void SetVelocity(float? x, float? y)
+    {
+        var velocity = _rigidbody2D.velocity;
+        x ??= velocity.x;
+        y ??= velocity.y;
+        _rigidbody2D.velocity = new Vector2(x.Value, y.Value);
+    }
+
+    public void SetState(string stateName)
+    {
+        _animator.Play(stateName);
+    }
+
+    public void SetFlipX(bool flipX)
+    {
+        _spriteRenderer.flipX = flipX;
+    }
+
+    public bool GetFlipX()
+    {
+        return _spriteRenderer.flipX;
+    }
+}
