82d76f0fdced4bd0d1da41a7f7ca7a9b6f87cd7c
alsdddk <alsdk__90@naver.com>
2023-11-16 10:57:18 +0900

    레벨 디자인 마무리
    trap 스크립트와 맵꾸는 ui 구현 후에....

---
+++ Unity/Assets/Script/Trap.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    //충돌한 순간 한 번 깎고, 1초 후애도 충돌 중이라면 또 깎고.....
+    private float DamageCoolTime; 
+    void Start()
+     {
+         
+     }
+    
+     void OnTriggerEnter(Collider other)
+     {
+         if (other.CompareTag("Player"))
+         {
+             Damage();
+         }
+     }
+     
+     void Damage()
+     {
+         // 피해를 가할 수 있는 시간 설정
+         nextDamageTime = Time.time + damageInterval;
+
+         // 플레이어에게 피해를 줌
+         playerHealth.TakeDamage(damageAmount);
+     }
+    void Update()
+    {
+        
+    }
+}
