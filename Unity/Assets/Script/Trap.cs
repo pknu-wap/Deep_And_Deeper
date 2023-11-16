242c5ba7f4306f54eb43d4f53e130b743b565f70
alsdddk <alsdk__90@naver.com>
2023-11-17 02:44:34 +0900

    Player Status 구현
    아직 스크립트는 X

--- Unity/Assets/Script/Trap.cs
+++ Unity/Assets/Script/Trap.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -31,4 +31,4 @@ public class Trap : MonoBehaviour
     {
         
     }
-}
+}*/
