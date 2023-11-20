79467e21c002964540ada9e16d3e500f76f6095a
이문빈 <3412mb@gmail.com>
2023-11-20 15:15:47 +0900

    Monster 클래스에 hp, speed 변수 추가

--- Unity/Assets/Monster/Monster.cs
+++ Unity/Assets/Monster/Monster.cs
@@ -4,6 +4,9 @@ namespace Monster
 {
     public class Monster : MonoBehaviour
     {
+        [SerializeField] private float hp = 1;
+        [SerializeField] private float speed = 1;
+
         private void Attack()
         {
             Debug.Log("attack!");
