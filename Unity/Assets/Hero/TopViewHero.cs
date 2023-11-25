3c6f3c76f36fe9b6296ae93212fcd8c5a0fda8da
이문빈 <3412mb@gmail.com>
2023-11-25 20:07:53 +0900

    TopViewHero Transform 이동을 RigidBody2D  이동으로 변경

--- Unity/Assets/Hero/TopViewHero.cs
+++ Unity/Assets/Hero/TopViewHero.cs
@@ -5,17 +5,19 @@ namespace Hero
 {
     public class TopViewHero : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 0.05f;
+        [SerializeField] private float moveSpeed = 5f;
 
         private Animator _animator;
         private SpriteRenderer _spriteRenderer;
         private Transform _transform;
+        private Rigidbody2D _rigidbody2D;
 
         private void Start()
         {
             _animator = GetComponent<Animator>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _transform = transform;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
@@ -26,17 +28,18 @@ namespace Hero
             if (hInput == 0 && vInput == 0)
             {
                 _animator.Play("Idle");
+                _rigidbody2D.velocity = Vector2.zero;
                 return;
             }
 
             if (hInput != 0) _spriteRenderer.flipX = (hInput < 0);
 
             _animator.Play("Run");
-            var hDirection = _transform.right * hInput;
-            var vDirection = _transform.up * vInput;
+            var hDirection = Vector2.right * hInput;
+            var vDirection = Vector2.up * vInput;
             var direction = hDirection + vDirection;
             direction.Normalize();
-            transform.Translate(direction * moveSpeed);
+            _rigidbody2D.velocity = direction * moveSpeed;
         }
     }
 }
