b212e6a4213b9dd13575ed5fa24b4e43acd985b3
이문빈 <3412mb@gmail.com>
2023-11-27 01:34:24 +0900

    스크립트 비활성화

--- Unity/Assets/Script/LivingEntity.cs
+++ Unity/Assets/Script/LivingEntity.cs
@@ -1,39 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LivingEntity : MonoBehaviour
 {
-    public float startingHealth; //나중에 수정해야 함.
-    public float health { get; protected set; }
-    public bool dead { get; protected set; }
-
-    protected virtual void OnEnable()
-    {
-        dead = false;
-        health = startingHealth;
-    }
-
-    public virtual void OnDamage(float damage)
-    {
-        health -= damage;
-        if (health <= 0 && !dead)
-        {
-            Die();
-        }
-    }
-
-    public virtual void Die()
-    {
-        dead = true;
-    }
-    
-    //체력회복
-    public virtual void RestoreHealth(float newHealth)
-    {
-        if (dead) return;
-
-        health += newHealth;
-    }
 }
--- Unity/Assets/Script/MonsterHealth.cs
+++ Unity/Assets/Script/MonsterHealth.cs
@@ -1,92 +1,86 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
 public class MonsterHealth : LivingEntity
 {
-    [SerializeField] private GameObject monsterHp; //프리팹
-    [SerializeField] private GameObject canvas;
-    private Image _healthBar;
-    private SpriteRenderer _spriteRenderer;
-    private RectTransform hpBar;
-    public bool isInTrigger;
-
-    private PlayerExp _playerExp;
-    private Money _playerMoney;
-    
-    [SerializeField] private float height = 1.7f; //MonsterHpBar가 머리 우에 오도록
-
-    private void Awake()
-    {
-        GameObject monsterHpBar = Instantiate(monsterHp, canvas.transform);
-        hpBar = monsterHpBar.GetComponent<RectTransform>();
-        startingHealth = 200f; //몬스터가 가진 체력 최대값 -> health가 현재 체력 값
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _healthBar = monsterHpBar.transform.GetChild(0).GetComponent<Image>();
-        isInTrigger = false;
-        
-        _playerExp = GameObject.FindWithTag("Player").GetComponent<PlayerExp>();
-        _playerMoney = GameObject.FindWithTag("Player").GetComponent<Money>();
-    }
-    
-    private void Update()
-    {
-        Vector3 _hpBarPos =
-            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0f));
-        hpBar.position = _hpBarPos;
-    }
-    
-    protected override void OnEnable()
-    {
-        base.OnEnable(); 
-
-        _healthBar.fillAmount = health / startingHealth;
-    }
-    public override void Die()
-    {
-        base.Die();
-        gameObject.SetActive(false); //나중에 수정
-
-        _playerExp.currentExp += 10f;
-        _playerExp.levelUp();
-
-        _playerMoney.currentMoney += 10;
-        _playerMoney.GetMoney();
-    }
-    
-    public override void OnDamage(float damage)
-    {
-        if (!dead)
-        {
-            StartCoroutine(FlashColor());
-        }
-        base.OnDamage(damage);
-        
-        float targetFillAmount = health / startingHealth;
-        _healthBar.fillAmount = targetFillAmount;
-    }
-    
-    IEnumerator FlashColor()
-    {
-        Color damagedColor = Color.red;
-        Color defaltColor = _spriteRenderer.color;
-        
-        _spriteRenderer.color = damagedColor;
-        
-        yield return new WaitForSeconds(0.2f);
-        _spriteRenderer.color = defaltColor;
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("PlayerAttack")) isInTrigger = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("PlayerAttack")) isInTrigger = false;
-    }
+    // [SerializeField] private GameObject monsterHp; //프리팹
+    // [SerializeField] private GameObject canvas;
+    // private Image _healthBar;
+    // private SpriteRenderer _spriteRenderer;
+    // private RectTransform hpBar;
+    // public bool isInTrigger;
+    //
+    // private PlayerExp _playerExp;
+    // private Money _playerMoney;
+    //
+    // [SerializeField] private float height = 1.7f; //MonsterHpBar가 머리 우에 오도록
+    //
+    // private void Awake()
+    // {
+    //     GameObject monsterHpBar = Instantiate(monsterHp, canvas.transform);
+    //     hpBar = monsterHpBar.GetComponent<RectTransform>();
+    //     startingHealth = 200f; //몬스터가 가진 체력 최대값 -> health가 현재 체력 값
+    //     _spriteRenderer = GetComponent<SpriteRenderer>();
+    //     _healthBar = monsterHpBar.transform.GetChild(0).GetComponent<Image>();
+    //     isInTrigger = false;
+    //     
+    //     _playerExp = GameObject.FindWithTag("Player").GetComponent<PlayerExp>();
+    //     _playerMoney = GameObject.FindWithTag("Player").GetComponent<Money>();
+    // }
+    //
+    // private void Update()
+    // {
+    //     Vector3 _hpBarPos =
+    //         Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0f));
+    //     hpBar.position = _hpBarPos;
+    // }
+    //
+    // protected override void OnEnable()
+    // {
+    //     base.OnEnable(); 
+    //
+    //     _healthBar.fillAmount = health / startingHealth;
+    // }
+    // public override void Die()
+    // {
+    //     base.Die();
+    //     gameObject.SetActive(false); //나중에 수정
+    //
+    //     _playerExp.currentExp += 10f;
+    //     _playerExp.levelUp();
+    //
+    //     _playerMoney.currentMoney += 10;
+    //     _playerMoney.GetMoney();
+    // }
+    //
+    // public override void OnDamage(float damage)
+    // {
+    //     if (!dead)
+    //     {
+    //         StartCoroutine(FlashColor());
+    //     }
+    //     base.OnDamage(damage);
+    //     
+    //     float targetFillAmount = health / startingHealth;
+    //     _healthBar.fillAmount = targetFillAmount;
+    // }
+    //
+    // IEnumerator FlashColor()
+    // {
+    //     Color damagedColor = Color.red;
+    //     Color defaltColor = _spriteRenderer.color;
+    //     
+    //     _spriteRenderer.color = damagedColor;
+    //     
+    //     yield return new WaitForSeconds(0.2f);
+    //     _spriteRenderer.color = defaltColor;
+    // }
+    //
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("PlayerAttack")) isInTrigger = true;
+    // }
+    //
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("PlayerAttack")) isInTrigger = false;
+    // }
 
 }
