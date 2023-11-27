e10cb7f42630cb51676446331d4c3c0f89b7dc21
이문빈 <3412mb@gmail.com>
2023-11-27 15:24:20 +0900

    DamageEnemies 노드 구현

--- Unity/Assets/Hero/Nodes/DamageEnemies.cs
+++ Unity/Assets/Hero/Nodes/DamageEnemies.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using MBT;
+using Monster;
 using UnityEngine;
 
 namespace Hero.Nodes
@@ -8,22 +10,49 @@ namespace Hero.Nodes
     [MBTNode(name = "Hero/DamageEnemies")]
     public class DamageEnemies : Leaf
     {
-        [SerializeField] private BoxCollider2D boxCollider2D;
         [SerializeField] private float damage = 50;
 
-        private Dictionary<BoxCollider2D, bool> _triggerDictionary;
+        private readonly Dictionary<Collider2D, bool> _triggerDictionary = new();
 
         public override NodeResult Execute()
         {
+            var removeList = new ArrayList();
+
+            foreach (var (key, value) in _triggerDictionary)
+            {
+                if (!value)
+                {
+                    removeList.Add(key);
+                    continue;
+                }
+
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                var monsterHealth = key.transform.parent.GetComponent<MonsterHealth>();
+
+                // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
+                if (monsterHealth == null)
+                {
+                    removeList.Add(key);
+                    continue;
+                }
+
+                monsterHealth.OnDamaged(damage);
+            }
+
+            foreach (Collider2D key in removeList)
+            {
+                _triggerDictionary.Remove(key);
+            }
+
             return NodeResult.success;
         }
 
-        private void OnTriggerEnter2D(BoxCollider2D other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             _triggerDictionary[other] = true;
         }
 
-        private void OnTriggerExit2D(BoxCollider2D other)
+        private void OnTriggerExit2D(Collider2D other)
         {
             _triggerDictionary[other] = false;
         }
