9824ed3e67274da2345ec6a806e49f17fa2a158d
alsdddk <alsdk__90@naver.com>
2023-11-28 02:37:26 +0900

    trap 스크립트
    - 그리드에 trap 오브젝트에는 "MeleeMonsterAttack" 부여
    - trap 스크립트의 OnTrigger함수에는 충돌하는 other이 player인지 체크

---
+++ Unity/Assets/Script/Trap.cs
@@ -0,0 +1,55 @@
+using Hero;
+using UnityEngine;
+
+namespace Script
+{
+    public class Trap : MonoBehaviour
+    {
+        [SerializeField] private float damage = 50;
+        [SerializeField] private float damageCooldown = 1f;
+
+        private bool _heroTrigger;
+        private bool _isReady = true;
+        private float _damageTimer;
+
+        private void Damage()
+        {
+            HeroManager.Instance.OnDamaged(damage);
+            _isReady = false;
+            _damageTimer = damageCooldown;
+        }
+
+        private void Update()
+        {
+            if (_isReady) return;
+
+            _damageTimer -= Time.deltaTime;
+
+            if (_damageTimer > 0) return;
+
+            _damageTimer = 0;
+            _isReady = true;
+
+            if (!_heroTrigger) return;
+
+            Damage();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            
+            _heroTrigger = true;
+
+            if (!_isReady) return;
+
+            Damage();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            _heroTrigger = false;
+        }
+    }
+}
