eb22c4de446d94dfd0f522153e4ed7439b7bca0b
이문빈 <3412mb@gmail.com>
2023-11-28 14:24:45 +0900

    아이템 컴포넌트 구현

--- Unity/Assets/Shop/Items/Item.cs
+++ Unity/Assets/Shop/Items/Item.cs
@@ -1,3 +1,5 @@
+using Hero;
+using TMPro;
 using UnityEngine;
 
 namespace Shop.Items
@@ -8,5 +10,25 @@ namespace Shop.Items
         [SerializeField] private int price;
         [SerializeField] private string itemName;
         [SerializeField] private string description;
+
+        private const string HasEnoughGold = "구매하려면 스페이스바를 눌러.";
+        private const string NotEnoughGold = "골드가 부족한 것 같은데?";
+        private TextMeshPro _itemText;
+
+        private void Start()
+        {
+            _itemText = GameObject.FindGameObjectWithTag("ItemText").GetComponent<TextMeshPro>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var additionalDescription = HeroManager.Instance.Money >= price ? HasEnoughGold : NotEnoughGold;
+            _itemText.text = $"{itemName}\n{description}\n{additionalDescription}";
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            _itemText.text = "";
+        }
     }
 }
