c3a11101b9d205ded5caba7a1187ec58cfd584ac
이문빈 <3412mb@gmail.com>
2023-12-01 12:00:37 +0900

    사운드매니저에 BGM 추가

--- Unity/Assets/Audio/SoundManager.cs
+++ Unity/Assets/Audio/SoundManager.cs
@@ -9,6 +9,12 @@ namespace Audio
         PlayerDead,
         MonsterHurt,
         MonsterDead,
+        Stage1,
+        Stage2,
+        Stage3,
+        Boss1,
+        Boss2,
+        Boss3,
     }
 
     public class SoundManager : MonoBehaviour
@@ -30,24 +36,32 @@ namespace Audio
             }
         }
 
-        private readonly Dictionary<Sound, AudioClip> _audioClips = new();
+        private readonly Dictionary<Sound, AudioContainer> _audioContainers = new();
         private readonly Dictionary<Sound, AudioSource> _audioSources = new();
 
         private void Awake()
         {
             var data = Resources.Load<SoundManagerData>("SoundManagerDataContainer");
 
-            _audioClips[Sound.PlayerHurt] = data.PlayerHurt;
-            _audioClips[Sound.PlayerDead] = data.PlayerDead;
-            _audioClips[Sound.MonsterHurt] = data.MonsterHurt;
-            _audioClips[Sound.MonsterDead] = data.MonsterDead;
+            _audioContainers[Sound.PlayerHurt] = data.PlayerHurt;
+            _audioContainers[Sound.PlayerDead] = data.PlayerDead;
+            _audioContainers[Sound.MonsterHurt] = data.MonsterHurt;
+            _audioContainers[Sound.MonsterDead] = data.MonsterDead;
+            _audioContainers[Sound.Stage1] = data.Stage1;
+            _audioContainers[Sound.Stage2] = data.Stage2;
+            _audioContainers[Sound.Stage3] = data.Stage3;
+            _audioContainers[Sound.Boss1] = data.Boss1;
+            _audioContainers[Sound.Boss2] = data.Boss2;
+            _audioContainers[Sound.Boss3] = data.Boss3;
 
             var prefab = data.audioSourcePrefab;
 
-            foreach (var (sound, audioClip) in _audioClips)
+            foreach (var (sound, audioContainer) in _audioContainers)
             {
                 var source = Instantiate(prefab).GetComponent<AudioSource>();
-                source.clip = audioClip;
+                source.clip = audioContainer.audioClip;
+                source.volume = audioContainer.volume;
+                source.loop = audioContainer.isLoop;
                 _audioSources[sound] = source;
             }
         }
--- Unity/Assets/Audio/SoundManagerData.cs
+++ Unity/Assets/Audio/SoundManagerData.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 namespace Audio
 {
+    [Serializable]
+    public class AudioContainer
+    {
+        public AudioClip audioClip;
+        public bool isLoop;
+        public float volume = 1;
+    }
+
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class SoundManagerData : MonoBehaviour
     {
-        public AudioClip PlayerHurt;
-        public AudioClip PlayerDead;
-        public AudioClip MonsterHurt;
-        public AudioClip MonsterDead;
+        public AudioContainer PlayerHurt;
+        public AudioContainer PlayerDead;
+        public AudioContainer MonsterHurt;
+        public AudioContainer MonsterDead;
+        public AudioContainer Stage1;
+        public AudioContainer Stage2;
+        public AudioContainer Stage3;
+        public AudioContainer Boss1;
+        public AudioContainer Boss2;
+        public AudioContainer Boss3;
         public GameObject audioSourcePrefab;
     }
 }
